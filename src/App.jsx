d490d8f3b35547ff0ab27d172dd5bf0f6181bb1a
thelight0804 <thelight0804@gmail.com>
2024-03-28 17:38:09 +0900

    Add: firebase install
    firebase 설치 후 FireStore DB 읽기 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import React from 'react'
+import { useEffect } from 'react'
 import './App.css'
 
+import db from './firebase/firebaseConfig';
+import { collection, getDocs } from "firebase/firestore";
+
 function App() {
+  const fetchData = async () => {
+    const querySnapshot = await getDocs(collection(db, "restaurant"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data().reviews[0]);
+    });
+  };
+
+  fetchData();
+
+
   return(
     <h1 className="text-3xl font-bold underline">
       Hello world!
