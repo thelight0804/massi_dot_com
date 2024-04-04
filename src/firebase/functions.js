2c063648585fd418d3b54fbb31154dcc4dcfe10c
thelight0804 <thelight0804@gmail.com>
2024-04-04 17:08:21 +0900

    Refactor: code to fetch Firestore data

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -1,15 +1,12 @@
 import db from '@/firebase/firebaseConfig';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore"; // Firestore 데이터 받아오기
 
-useEffect(() => {
-  const fetchData = async () => {
-    const querySnapshot = await getDocs(collection(db, "restaurant"));
-    querySnapshot.forEach((doc) => {
-      console.log(doc.data());
-    });
-  };
-
-  fetchData();
-}, []);
-
-fetchData();
+/**
+ * Firestore 데이터 받아오기
+ */
+const fetchData = async () => {
+  const querySnapshot = await getDocs(collection(db, "restaurant"));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+  });
+};
