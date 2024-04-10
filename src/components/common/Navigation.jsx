1d83b7ec3cde85db42204a1c4bedb7b9b6920ff5
thelight0804 <thelight0804@gmail.com>
2024-04-10 20:34:37 +0900

    Refactor: Navigation 메뉴 위치 이동

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -5,13 +5,17 @@ import * as ROUTE from "@/constants/routes";
 const Navigation = () => {
 
   return (
-    <nav class="flex items-center p-3 bg-blue-500 font-do-hyeon">
-      <div class="text-white mr-3">
-        <Link to="/">맛있닷컴</Link>
+    <nav class="flex items-center p-3 font-do-hyeon justify-between">
+      <div class="flex items-center space-x-4">
+        <div class="text-xl">
+          <Link to="/">맛있닷컴</Link>
+        </div>
+        <ul class="flex space-x-4">
+          <li><NavLink to={ROUTE.Restaurant}>가게</NavLink></li>
+          <li><NavLink to={ROUTE.Review}>리뷰</NavLink></li>
+        </ul>
       </div>
-      <ul class="flex space-x-4">
-        <li><NavLink to={ROUTE.Restaurant}>가게</NavLink></li>
-        <li><NavLink to={ROUTE.Review}>리뷰</NavLink></li>
+      <ul class="bg">
         <li><NavLink to={ROUTE.MyPage}>마이 페이지</NavLink></li>
       </ul>
     </nav>
