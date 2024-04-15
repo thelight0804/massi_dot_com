0d0446eca76cd90934418c4dbca0610f805b044d
thelight0804 <thelight0804@gmail.com>
2024-04-15 11:39:12 +0900

    Refactor: Update Navigation component with logo image
    Navigation에 로고 이미지 추가

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import * as ROUTE from "@/constants/routes";
+import logo from "@/images/logo-full.png";
 
 const Navigation = () => {
 
   return (
     <nav class="flex items-center p-3 font-do-hyeon justify-between border-b-2 border-stone-300">
       <div class="flex items-center space-x-4">
-        <div class="text-xl">
-          <Link to="/">맛있닷컴</Link>
+        <div class="flex items-center justify-center w-24 h-auto">
+          <Link to="/">
+            <img src={logo} alt="logo"/>
+          </Link>
         </div>
         <ul class="flex space-x-4">
           <li><NavLink to={ROUTE.Review}>메뉴</NavLink></li>
