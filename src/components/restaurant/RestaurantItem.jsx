67a6cfee291aa57242537a82781bb232c9b33089
thelight0804 <thelight0804@gmail.com>
2024-04-17 22:00:23 +0900

    Fix: Invalid DOM property warning class names to use className instead of class
    HTML属性をclassからclassNameに修正して、Invalid DOM property warningエラー解決
    
    HTML 속성을 class에서 className으로 수정하여 Invalid DOM property warning 해결

--- src/components/restaurant/RestaurantItem.jsx
+++ src/components/restaurant/RestaurantItem.jsx
@@ -1,32 +1,35 @@
 import React from "react";
+import * as ROUTE from "@/constants/routes";
 import { IconContext } from "react-icons";
 import { FaStar } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const RestaurantItem = ({restaurant}) => {
+  const navigate = useNavigate();
 
   const onClickRestaurant = () => {
-    console.log(restaurant.id);
+    navigate(ROUTE.ViewRestaurant.replace(':id', restaurant.id), {state: {restaurant}})
   };
 
   return (
     <button 
-      class="flex items-center border-b-2 border-stone-100 py-4 w-full hover:bg-amber-100"
+      className="flex items-center border-b-2 border-stone-100 py-4 w-full hover:bg-amber-100"
       onClick={onClickRestaurant}
     >
-      <div class="h-50 w-auto m-5">
+      <div className="h-50 w-auto m-5">
         <img src={restaurant.image} alt="restaurant"/>
       </div>
       <div>
-        <h1 class="font-bold text-lg">{restaurant.name}</h1>
-        <div class="flex items-center">
+        <h1 className="font-bold text-lg">{restaurant.name}</h1>
+        <div className="flex items-center">
           <IconContext.Provider value={{color: "orange"}}>
             <FaStar/>
-            <p class="font-bold">{restaurant.avgRating}</p>
+            <p className="font-bold">{restaurant.avgRating}</p>
           </IconContext.Provider>
         </div>
         <p>{restaurant.info.phoneNumber}</p>
         <div>
-          <span class="text-stone-500">대표 매뉴</span>
+          <span className="text-stone-500">대표 메뉴</span>
           <span>&nbsp;{restaurant.mainMenu}</span>
         </div>
       </div>
