2b5630ac5f4eeee98cdaab22f4384c991715b1ac
eunji1107 <93811062+20203207@users.noreply.github.com>
2024-04-18 15:33:01 +0900

    Update index.jsx

--- src/views/review/index.jsx
+++ src/views/review/index.jsx
@@ -55,7 +55,7 @@ function Tab() {
             <div class="w-1/2 p-4">
               <p class="text-lg"><strong>두마리치킨</strong></p>
               <p class="text-sm">[후라이드1+선택1]</p>
-              <p class="text-sm">19,900원sdfsdf</p>
+              <p class="text-sm">19,900원</p>
             </div>
             <div class="w-1/2 p-4 flex justify-end">
               <img src="https://media.istockphoto.com/id/1439803916/ko/%EC%82%AC%EC%A7%84/%EA%B5%AC%EC%9A%B4-%EB%8B%AD-%EB%82%A0%EA%B0%9C.jpg?s=612x612&w=is&k=20&c=GXllb_4NaJhmFw-LrCkAVDdH0MqwSmlYtEKnvBqRr2Y="
