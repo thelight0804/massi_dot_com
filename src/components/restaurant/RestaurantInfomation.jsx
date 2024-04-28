8198e9d08dabda6f12830fc88ed6e238fcdff7e4
rox120 <wjdgusdn48@gmail.com>
2024-04-28 22:32:27 +0900

    Update: RestaurantInformation.jsx
    - 가게 소개
    * 테이블 너비 자동 조정으로 변경

--- src/components/restaurant/RestaurantInfomation.jsx
+++ src/components/restaurant/RestaurantInfomation.jsx
@@ -5,7 +5,7 @@ const RestaurantInfomation = ({ name, info }) => {
     <div className="bg-white shadow-md space-y-10"> {/* bg-white: 배경색, shadow-md: 그림자 */}
       <div className="bg-white shadow-md">
         <h2 className="px-4 text-2xl font-bold mb-2">{name}</h2> {/* px-4: padding, text-2xl: 글자 크기, font-bold: 글자 굵기 */}
-        <table className="table-auto w-full"> {/* table-auto: 테이블 크기 자동 조절, w-full: 테이블 가로폭 100% */}
+        <table className="table-auto w-auto"> {/* table-auto: 테이블 크기 자동 조정, w-auto: 너비 자동 조정 */}
           <tbody>
             <tr>
               <td className="px-5 py-5 text-gray-700 font-bold">주소</td> {/* px-5: padding, py-5: padding, text-gray-700: 글자색 */}
@@ -35,31 +35,6 @@ const RestaurantInfomation = ({ name, info }) => {
             </tr>
           </tbody>
         </table>
-      </div>
-      <div className="bg-white shadow-md">
-        <h2 className="px-4 text-2xl font-bold mb-2">가게 소개</h2>
-        <table className="table-auto w-full">
-          <tbody>
-            <tr>
-              <td className="px-5 py-5 text-gray-700">엄마의 손맛으로 최고의 맛을 선사합니다.</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className="bg-white shadow-md">
-        <div className="mb-10"> {/* mb-10: 배민 어플에서 마지막 정보 하단에 여백을 길게 줌. */}
-          <h2 className="px-4 text-2xl font-bold mb-2">안내 및 혜택</h2>
-          <table className="table-auto w-full">
-            <tbody>
-              <tr>
-                <td className="px-5 py-5 text-gray-700">
-                  <p>빠른 배송을 위해 아파트명 건물명이나 숙박업소 이름을 꼭 적어주세요!!</p><br></br> {/* <br>: 줄바꿈 */}
-                  <p>환경보호 차원에서 앱 주문 시 '일회용 수저, 포크 안 받기' 기본 설정 적용 되어 있습니다. 체크 잘하셔서 불편함 없도록 신경 써주시기 바랍니다.</p>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
       </div>
     </div>
   );
