fc11cb3415ecef6548cea1c9e2afd61bf0be0a25
rox120 <wjdgusdn48@gmail.com>
2024-05-09 15:30:04 +0900

    Update: RestaurantReviews.jsx
    * 유저 프로필 이미지와 유저 이름 같은 선상에 배치
    * 리뷰 이미지(음식 사진) 리뷰 내용 아래에 위치하도록 배치
    * 회색 말풍선 색깔 gray -> slate로 변경, 사장님 말풍선 크기 조정

--- src/components/restaurant/RestaurantReviews.jsx
+++ src/components/restaurant/RestaurantReviews.jsx
@@ -6,7 +6,7 @@ const StarRating = ({ rating }) => {
   {/* 별점을 표시하는 컴포넌트 */ }
   let stars = [];
   for (let i = 0; i < rating; i++) {
-    stars.push(<span key={i} className="text-orange-500">
+    stars.push(<span key={i} className="text-orange-400">
       <FaStar />
     </span>);
   }
@@ -16,31 +16,37 @@ const StarRating = ({ rating }) => {
 const RestaurantReviews = ({ name, reviews }) => {
   return (
     <div>
-      <div className="text-2xl font-bold">{name}</div>
+      <div className="text-2xl font-bold ml-1.5">{name}</div> {/* 가게 이름 */}
       <div> {/* 리뷰 목록 */}
         {/* map 함수로 reviews 배열을 순회하며 리뷰 목록을 출력 */}
         {reviews.map((review, index) => (
           <div key={index}>
             <div className="flex">
               <div className="w-1/2 p-4">
-                <img src={review.profileImage} className="w-32 h-auto rounded-xl" /> {/* 프로필 이미지 */}
-                <p className="text-lg font-bold">{review.userName}</p> {/* 유저 이름 */}
+                <div className="flex items-center">
+                  <img src={review.profileImage} className="w-5 h-5 rounded-xl mr-1" /> {/* 프로필 이미지 */}
+                  <p className="text-lg font-bold">{review.userName}</p> {/* 유저 이름 */}
+                </div>
                 <p className="text-sm flex items-center"> {/* 별점과 날짜 */}
                   <div className="mr-2.5"> {/* 별점 */}
                     <StarRating rating={review.rating} /> {/* StarRating 컴포넌트 */}
                   </div>
                   <span>{review.date}</span> {/* 날짜 */}
                 </p>
-                <p className="text-sm">{review.content}</p> {/* 리뷰 내용 */}
-                <p className="text-sm inline-block bg-gray-200 p-1.5 rounded-lg">
-                  {review.eatenMenu} {/* 메뉴 이름 */}
-                </p>
-              </div>
-              <div className="w-1/2 p-4 flex justify-end"> {/* 이미지 */}
-                <img src={review.image} alt={review.name} className="w-32 h-auto rounded-xl" /> {/* 리뷰 이미지 */}
+                <div className="flex flex-col"> {/* 리뷰 내용과 이미지 */}
+                  <div className="w-full p-4"> 
+                    <p className="text-sm">{review.content}</p> {/* 리뷰 내용 */}
+                    <p className="text-sm inline-block bg-slate-200 p-1.5 rounded-lg mt-1">
+                      {review.eatenMenu} {/* 메뉴 이름 */}
+                    </p>
+                    <div className="w-1/2 p-4 flex justify-end"> {/* 리뷰 이미지 */}
+                      <img src={review.image} alt={review.name} className="w-32 h-auto rounded-xl" />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <p className="text-sm pl-10 bg-gray-200 p-2.5 rounded-lg relative max-w-4/5 ml-5 shadow-md">
+            <p className="text-sm bg-slate-200 p-2.5 rounded-lg relative max-w-screen-lg ml-7">
               {review.reply && ( // 답글이 존재하면 출력
                 <>
                   <p className="text-lg font-bold">사장님</p>
