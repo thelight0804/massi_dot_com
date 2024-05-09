8c413b6c3ba816814048b55a9519e9ac22a00e80
rox120 <wjdgusdn48@gmail.com>
2024-05-09 14:28:56 +0900

    Update: RestaurantReviews.jsx
    * style 코드를 모두 tailwind 코드로 대체했음.

--- src/components/restaurant/RestaurantReviews.jsx
+++ src/components/restaurant/RestaurantReviews.jsx
@@ -6,15 +6,14 @@ const StarRating = ({ rating }) => {
   {/* 별점을 표시하는 컴포넌트 */ }
   let stars = [];
   for (let i = 0; i < rating; i++) {
-    stars.push(<span key={i}>
-      <IconContext.Provider value={{ color: "orange" }}>
-        <FaStar />
-      </IconContext.Provider></span>);
+    stars.push(<span key={i} className="text-orange-500">
+      <FaStar />
+    </span>);
   }
-  return <div style={{ display: 'flex' }}>{stars}</div>; 
+  return <div className="flex">{stars}</div>;
 };
 
-const RestaurantReviews = ({ name, reviews }) => { 
+const RestaurantReviews = ({ name, reviews }) => {
   return (
     <div>
       <div className="text-2xl font-bold">{name}</div>
@@ -26,8 +25,8 @@ const RestaurantReviews = ({ name, reviews }) => {
               <div className="w-1/2 p-4">
                 <img src={review.profileImage} className="w-32 h-auto rounded-xl" /> {/* 프로필 이미지 */}
                 <p className="text-lg font-bold">{review.userName}</p> {/* 유저 이름 */}
-                <p className="text-sm" style={{ display: 'flex', alignItems: 'center' }}> {/* 별점과 날짜 */}
-                  <div style={{ marginRight: '10px' }}> {/* 별점 */}
+                <p className="text-sm flex items-center"> {/* 별점과 날짜 */}
+                  <div className="mr-2.5"> {/* 별점 */}
                     <StarRating rating={review.rating} /> {/* StarRating 컴포넌트 */}
                   </div>
                   <span>{review.date}</span> {/* 날짜 */}
@@ -41,33 +40,16 @@ const RestaurantReviews = ({ name, reviews }) => {
                 <img src={review.image} alt={review.name} className="w-32 h-auto rounded-xl" /> {/* 리뷰 이미지 */}
               </div>
             </div>
-            <p className="text-sm" style={{ /* 사장님 답글 */
-              paddingLeft: '40px', 
-              backgroundColor: '#f2f2f2', 
-              padding: '10px', 
-              borderRadius: '10px',
-              position: 'relative',
-              maxWidth: '80%',
-              marginLeft: '20px',
-              boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)'
-            }}>
+            <p className="text-sm pl-10 bg-gray-200 p-2.5 rounded-lg relative max-w-4/5 ml-5 shadow-md">
               {review.reply && ( // 답글이 존재하면 출력
                 <>
-                  <p className="text-lg font-bold">사장님</p> 
+                  <p className="text-lg font-bold">사장님</p>
                   {review.reply} {/* 답글 내용 */}
-                  <span style={{
-                    content: '',
-                    position: 'absolute',
-                    left: '-10px',
-                    top: '10px',
-                    width: '0',
-                    height: '0',
-                    borderTop: '10px solid transparent',
-                    borderBottom: '10px solid transparent',
-                    borderRight: `10px solid #f2f2f2`,
-                  }}></span>
+                  <span className="absolute border-t border-b border-r 
+                                  -left-2.5 top-2.5 w-0 h-0 
+                                  border-transparent">
+                  </span>
                 </>
-              
               )}
             </p>
           </div>
