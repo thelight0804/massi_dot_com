03b1566260c4749545e3dddb6c921f695842ebe8
thelight0804 <thelight0804@gmail.com>
2024-05-24 17:33:57 +0900

    Update: Review design
    리뷰 페이지 디자인 수정

--- src/components/restaurant/RestaurantReviews.jsx
+++ src/components/restaurant/RestaurantReviews.jsx
@@ -1,68 +1,75 @@
-import react from 'react';
+import React from "react";
 import { IconContext } from "react-icons";
-import { FaStar } from "react-icons/fa";
-
-const StarRating = ({ rating }) => {
-  {/* 별점을 표시하는 컴포넌트 */ }
-  let stars = [];
-  for (let i = 0; i < rating; i++) {
-    stars.push(<span key={i} className="text-orange-400">
-      <FaStar />
-    </span>);
-  }
-  return <div className="flex">{stars}</div>;
-};
+import { FaStar, FaRegUser } from "react-icons/fa";
 
 const RestaurantReviews = ({ name, reviews }) => {
   return (
     <div>
-      <div className="text-2xl font-bold ml-1.5">{name}</div> {/* 가게 이름 */}
+      <div className="ml-1.5 text-2xl font-bold">{name}</div> {/* 가게 이름 */}
       <div> {/* 리뷰 목록 */}
-        {/* map 함수로 reviews 배열을 순회하며 리뷰 목록을 출력 */}
-        {reviews.map((review, index) => (
-          <div className="md:flex" key={index}>
-            <div className="flex justify-around">
-              <div className="w-full p-4">
-                <div className="flex items-center">
-                  <img src={review.profileImage} className="w-5 h-5 rounded-xl mr-1" /> {/* 프로필 이미지 */}
-                  <p className="text-lg font-bold">{review.userName}</p> {/* 유저 이름 */}
-                </div>
-                <p className="text-sm flex items-center"> {/* 별점과 날짜 */}
-                  <div className="mr-2.5"> {/* 별점 */}
-                    <StarRating rating={review.rating} /> {/* StarRating 컴포넌트 */}
-                  </div>
-                  <span>{review.date}</span> {/* 날짜 */}
-                </p>
-                <div className="flex flex-col"> {/* 리뷰 내용과 이미지 */}
-                  <div className="w-full p-4"> 
-                    <p className="text-sm">{review.content}</p> {/* 리뷰 내용 */}
-                    <p className="text-sm inline-block bg-slate-200 p-1.5 rounded-lg mt-1">
-                      {review.eatenMenu} {/* 메뉴 이름 */}
-                    </p>
-                    <div className="w-1/2 p-4 flex justify-end"> {/* 리뷰 이미지 */}
-                      <img src={review.image} alt={review.name} className="w-32 h-auto rounded-xl" />
+        {reviews.map((review, index) => ( // map 함수로 reviews 배열을 순회하며 리뷰 목록을 출력
+          <div key={index}>
+            <div className="w-full p-4">
+              <div className="flex items-center">
+                {review.profileImage ? (
+                  <img src={review.profileImage} className="mr-1 h-8 w-8 rounded-xl" />
+                ) : (
+                  <IconContext.Provider value={{ color: "#fca5a5" }}>
+                    <FaRegUser className="mr-1 h-8 w-8 rounded-xl" />
+                  </IconContext.Provider>
+                ) }
+                <div>
+                  <p className="font-bold">{review.userName}</p>
+                  <div className="flex items-center">
+                    <div className="flex items-center">
+                      <div className="mr-2.5">
+                        <StarRating rating={review.rating} />
+                      </div>
+                      <p className="text-gray-500">{review.date}</p>
                     </div>
                   </div>
                 </div>
               </div>
+              <p className="text-sm">{review.content}</p>
+              <p className="mt-1 inline-block rounded-lg bg-red-100 p-2 text-sm">
+                {review.eatenMenu}
+              </p>
+              {review.images && ( // 이미지가 존재하면 출력
+                  <div className="flex flex-wrap">
+                    {review.images.map((image, index) => (
+                      <img key={index} src={image} alt={review.name} className="h-auto w-32 rounded-xl mr-2.5 mb-2.5" />
+                    ))}
+                  </div>
+                )
+              }
             </div>
-            <p className="text-sm bg-slate-200 p-2.5 rounded-lg relative w-full ml-7">
-              {review.reply && ( // 답글이 존재하면 출력
-                <>
-                  <p className="text-lg font-bold">사장님</p>
-                  {review.reply} {/* 답글 내용 */}
-                  <span className="absolute border-t border-b border-r 
-                                  -left-2.5 top-2.5 w-0 h-0 
-                                  border-transparent">
-                  </span>
-                </>
-              )}
-            </p>
+            {review.reply && ( // 답글이 존재하면 출력
+              <div className="rounded-lg bg-orange-100 p-4 ml-8 mr-4 text-sm">
+                <p className="font-bold">사장님</p>
+                <p>{review.reply}</p>
+              </div>
+            )}
+            <div className="h-0.5 m-4 bg-gray-100" />
           </div>
         ))}
       </div>
     </div>
   );
+};
+
+/**
+ * 별점을 표시하는 컴포넌트
+ */
+const StarRating = ({ rating }) => {
+  let stars = [];
+  for (let i = 0; i < rating; i++) {
+    stars.push(
+      <span key={i} className="text-orange-400">
+        <FaStar />
+      </span>,
+    );
+  }
+  return <div className="flex">{stars}</div>;
 };
 
 export default RestaurantReviews;
